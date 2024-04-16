0d2434769831d1eb4b8d1964fb94b11fd2ef0969
Maciek12pl <156930765+Maciek12pl@users.noreply.github.com>
2024-04-16 20:58:12 +0200

    Update User.java

--- WAP/wap-initial-project/src/main/java/com/uep/wap/model/User.java
+++ WAP/wap-initial-project/src/main/java/com/uep/wap/model/User.java
@@ -9,8 +9,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private long id;
-    @Column(name = "name")
-    private String name;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "last_name")
+    private String last_name;
     @Column(name = "email")
     private String email;
 
@@ -26,11 +28,17 @@ public class User {
     public void setId(long id) {
         this.id = id;
     }
-    public String getName() {
-        return name;
+    public String getFirst_name() {
+        return first_name;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
     public String getEmail() {
         return email;
